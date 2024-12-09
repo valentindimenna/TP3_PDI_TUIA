e3adbe1f6037e34b3ddbb3ec6212ccafbd7c160e
Valentin Dimenna <valentindimenna@gmail.com>
2024-12-09 19:05:38 -0300

    Correcciones en dados, agregados los videos de salida

--- tiradas_dados.py
+++ tiradas_dados.py
@@ -18,141 +18,170 @@ def imshow(img, new_fig=True, title=None, color_img=False, blocking=False, color
         plt.show(block=blocking)
 def segmentar(imagen, margen_inferior:np.array, margen_superior:np.array):
     """
-    Recibe una imaggen en BGR y dos margenes y devuelve una mascara de todos los pixeles que tienen valores entre los margenes
+    Recibe una imagen y dos margenes y devuelve una mascara en escala de grises 
+    poniedo a todos los pixeles que tienen valores entre esos margenes un valor  de 255
     """
     mask = cv2.inRange(imagen, margen_inferior, margen_superior)
-    
     return mask
-def detectar_dados_movimientos():
-    pass
-
-
-img_path = "videos_dados/tirada_4.mp4"
-# img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-imag = cv2.VideoCapture(img_path)
-frames=[]
-
-while imag.isOpened():
-    ret,img = imag.read()
-    if ret == False:
-        break
-    frames.append(img)
-    cv2.imshow('Iamgen',img)
-    
-plt.imshow(frames[75])
-plt.show()
-prueba = frames[75]
-limite_inferior = np.array([80, 0, 0])  # Limite Inferior de cada Canal
-limite_superior = np.array([255, 50, 70])# Limite Superior de cada Canal
-# mask = cv2.inRange(imghsv, limite_inferior, limite_superior)
-# result = cv2.bitwise_and(img,img,mask=mask)
-mask = segmentar(prueba,limite_inferior,limite_superior)
-result = cv2.bitwise_and(prueba, prueba, mask=mask)
-plt.imshow(mask,cmap='gray')
-plt.show()
-###Leo y guardo todos los frames del video en una lista
-
-img_path = "videos_dados/tirada_1.mp4"
-# img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-imag = cv2.VideoCapture(img_path)
-frames = []
-frames_gr = []
-i=0
-while True:
-    ret,frame = imag.read()
-    if not ret:
-        break
-    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    frame_gr = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    frames_gr.append(frame_gr)
-    frames.append(frame)
-    # if i>50:
-        # plt.imshow(frame_gr,cmap='gray')
+def leer_video(ruta_video):
+    limite_inferior_rojo = np.array([75, 0, 0])
+    limite_superior_rojo = np.array([255, 65, 75])
+    limite_inferior_verde = np.array([0, 0, 0])
+    limite_superior_verde = np.array([165, 215,180])
+    limite_inferior_verde_hsv = np.array([65, 180, 60])
+    limite_superior_verde_hsv = np.array([95, 255,215])
+    # img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    imag = cv2.VideoCapture(ruta_video)
+    frames = []
+    frames_gr = []
+    frames_hsv =[]
+    i=0
+    while True:
+        ret,frame = imag.read()
+        if not ret:
+            break
+        frame_hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        if i == 0:
+            mascara = segmentar(frame_hsv,limite_inferior_verde_hsv,limite_superior_verde_hsv)
+            i += 1
+        
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        # plt.imshow(frame)
         # plt.show()
-        # print(i)
-    i+=1
-
-limite_inferior = np.array([80, 0, 0])  # Limite Inferior de cada Canal
-limite_superior = np.array([255, 50, 70])# Limite Superior de cada Canal
-frames_consecutivos = 0
-# for i in range(len(frames)):
-
-
-frames_consecutivos = 0
-i=0
-while True:
-    # plt.figure()
-    # ax1 = plt.subplot(241); plt.xticks([]), plt.yticks([]), plt.imshow(frames[i]), plt.title('primero')
-    # plt.subplot(242,sharex=ax1,sharey=ax1), plt.imshow(frames[i+1], cmap="gray"), plt.title('Segundo')
-    # plt.show(block=False)
-    diff = cv2.absdiff(frames_gr[67], frames_gr[80])
-    _, diff_thresh = cv2.threshold(diff, 90, 255, cv2.THRESH_BINARY)
-    plt.figure()
-    ax1 = plt.subplot(241); plt.xticks([]), plt.yticks([]), plt.imshow(frames_gr[67],cmap='gray'), plt.title('primero')
-    plt.subplot(242,sharex=ax1,sharey=ax1), plt.imshow(frames_gr[77], cmap="gray"), plt.title('Segundo')
-    plt.subplot(243,sharex=ax1,sharey=ax1), plt.imshow(diff, cmap="gray"), plt.title('Segundo')
-    plt.subplot(244,sharex=ax1,sharey=ax1), plt.imshow(diff_thresh, cmap="gray"), plt.title('Segundo')
-    plt.show(block=False)
-    # plt.imshow(diff,cmap='gray')
-    # plt.show()
-    print(diff)
-    # _, diff_thresh = cv2.threshold(diff, 90, 255, cv2.THRESH_BINARY)
-    # plt.imshow(diff_thresh,cmap='gray')
-    # plt.show()
-    motion_intensity = np.sum(diff_thresh)
-    print(motion_intensity)
-    break
-    mascara1 = segmentar(frames[i+5],limite_inferior,limite_superior)
-    if i < (len(frames)-1):
-        mascara2 = segmentar(frames[i+20],limite_inferior,limite_superior)
-    else:
-        i+=1
-        continue
-    # plt.figure()
-    # ax1 = plt.subplot(241); plt.xticks([]), plt.yticks([]), plt.imshow(mascara1,cmap = 'gray'), plt.title('Mascara1')
-    # plt.subplot(242,sharex=ax1,sharey=ax1), plt.imshow(mascara2, cmap="gray"), plt.title('Segundo')
-    # plt.show(block=False)
-    similarity = np.sum(mascara1 == mascara2) / mascara1.size
-    if similarity > 0.99 and not (mascara1 == np.zeros(mascara1.shape)).all():
-        print("Máscaras similares")
-        frames_consecutivos += 1
-    else:
-        print("Reseteo")
-        frames_consecutivos = 0
-    # if (mascara1 == mascara2).all() and not (mascara1 == np.zeros(mascara1.shape)).all():
-    #     print("entro")
-    #     frames_consecutivos += 1
-    # else:
-    #     print("Reseteo")
-    #     frames_consecutivos = 0
-    # print("el i ")
-    i += 1
-    # print("se incrementa")
-# Frame en el que se frenan = i 
-img_path = "videos_dados/tirada_1.mp4"
-# img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-imag = cv2.VideoCapture(img_path)
-frames = []
-frames_gr = []
-i=0
-while True:
-    ret,frame = imag.read()
-    if not ret:
-        break
-    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    frame_gr = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    frames_gr.append(frame_gr)
-    frames.append(frame)
-
-
-
-for i in range(len(frames_gr)):
-    print(i)
-    frame1 = frames_gr[i]
-    frame2 = frames_gr[i+13]
-    diff = cv2.absdiff(frame1, frame2)
-    _, diff_thresh = cv2.threshold(diff, 90, 255, cv2.THRESH_BINARY)
-    motion_intensity = np.sum(diff_thresh)
-    if motion_intensity < 45000:
-        break
-frenos.append(i)
+        masked_frame = cv2.bitwise_and(frame, frame, mask = mascara)
+        frames.append(masked_frame)
+        frame_gr = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        masked_frame_gr = cv2.bitwise_and(frame_gr, frame_gr, mask = mascara)
+        frames_gr.append(masked_frame_gr)
+        frames_hsv.append(frame_hsv)
+        # plt.imshow(masked_frame)
+        # plt.show()
+        # cv2.imshow('frame',masked_frame)
+        # cv2.imshow('movimiento', frame_hsv)
+    #     if cv2.waitKey(1) == ord('q'):
+    #         break 
+    # imag.release()
+    # cv2.destroyAllWindows()
+    return frames_hsv,frames,frames_gr
+##############ESRE ES EL DE AHORA
+limite_inferior_rojo = np.array([75, 0, 0])
+limite_superior_rojo = np.array([255, 65, 75])
+limite_inferior_verde = np.array([0, 0, 0])
+limite_superior_verde = np.array([165, 215,180])
+limite_inferior_verde_hsv = np.array([65, 180, 60])
+limite_superior_verde_hsv = np.array([95, 255,215])
+for tirada in range(1,5):
+    frameshsv,frames,frames_gr = leer_video(f"videos_dados/tirada_{tirada}.mp4")
+    frame_min = 40
+    freno = 0
+    band = 0
+    band2 = 0
+    movimiento = len(frames_gr) - 1 
+    for i in range(len(frames_gr)-2):
+        frame1 = segmentar(frames[i],limite_inferior_rojo,limite_superior_rojo)
+        frame2 = segmentar(frames[i+1],limite_inferior_rojo,limite_superior_rojo)
+        frame3 = segmentar(frames[i+2],limite_inferior_rojo,limite_superior_rojo)
+        # plt.imshow(frame1)
+        # plt.show()
+        # frame1=cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+        # frame2=cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+        # frame1=frames_gr[i]
+        # frame2=frames_gr[i+1]
+        diff = cv2.absdiff(frame1,frame2)
+        # diff_gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+        _, diff_thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
+        # plt.imshow(diff_thresh,cmap = 'gray')
+        # plt.show()
+        movement = np.sum(diff_thresh > 0)
+        diff2 = cv2.absdiff(frame2,frame3)
+        _, diff_thresh2 = cv2.threshold(diff2, 25, 255, cv2.THRESH_BINARY)
+        movement2 = np.sum(diff_thresh2 > 0)
+        if movement < 2000 and i > frame_min and band == 0 and movement2 <2000:
+            # print("ACA 2",i)
+            band=1
+            freno = i
+        elif movement > 2000 and movement2 > 2000 and band==1 and band2==0:
+            # print("ACA",i)
+            band2 = 1
+            movimiento = i
+            break
+        # print(freno)
+        # print(movimiento)
+        # print(i, ":" ,movement)
+    #Freno: frame en el que deja de haber movimiento
+    #Movimiento: Frame en el que vuelve a haber movimiento o ultimo frame del video
+    salida = []
+    for i in range(len(frames)):
+        if i < freno or i > movimiento:
+            salida.append(frames[i])
+        else:
+            im = frames[i]
+            mascara = segmentar(im, limite_inferior_rojo,limite_superior_rojo)
+            # plt.imshow(mascara,cmap='gray')
+            # plt.show()
+            result = cv2.bitwise_and(im, im, mask = mascara)
+            # plt.imshow(mascara,cmap='gray')
+            # plt.show()
+            contours, jerarquia = cv2.findContours(mascara, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            # for j,cnt in enumerate(contours):
+            #     print(cv2.contourArea(cnt))
+            filtered_contours = [(i,cnt) for i,cnt in enumerate(contours) if cv2.contourArea(cnt) > 100]
+            dados = []
+            indices = []
+            for i,cnt in filtered_contours:
+                # Crear un rectángulo alrededor del contorno
+                x, y, w, h = cv2.boundingRect(cnt)
+                indices.append(i)
+                # Recortar el objeto en la imagen original en color
+                objeto = result[y:y+h, x:x+w]
+                # Mostrar el objeto
+                dados.append(objeto)
+                # plt.imshow(objeto)
+                # plt.title("Contornos encontrados")
+                # plt.show()
+            dict_contornos = {}
+            padres = 0
+            hijos = 0
+            conteo_dado = 0
+            copia = im.copy()
+            for indice in indices:
+                _, _, first_child, parent = jerarquia[0][indice]
+                # print(indice,jerarquia[0][indice])
+                # print(first_child,parent)
+                if parent == -1:
+                    #Es padre 
+                    padres +=1 
+                    color = (0, 0, 255)  # Azul para contornos padres
+                    dict_contornos[indice] = []  # Inicializo la lista de hijos para este padre
+                else:  
+                    # Contorno hijo
+                    hijos += 1
+                    color = (0, 0, 255)  # Azul para contornos hijos
+                    # Agregar este contorno hijo a la lista del contorno padre
+                    if parent in dict_contornos:
+                        dict_contornos[parent].append(indice)
+                conteo_dado = hijos
+                cv2.drawContours(copia, contours, indice, color, 1)
+            conteo_dados = hijos
+            for parent_index in dict_contornos:
+                # Obtenemos las coordenadas del rectángulo que envuelve el dado
+                if len(dict_contornos[parent_index])==0:
+                    continue
+                x, y, w, h = cv2.boundingRect(contours[parent_index])
+                # Asignamos el número de puntos detectados en el dado (hijos)
+                numero = len(dict_contornos[parent_index])
+                # Escribimos el número sobre el dado
+                cv2.putText(copia, str(numero), (x + w // 2, y + h // 2), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 0, 255), 2)
+            # salida += [copia for _ in range(movimiento - freno)]# Si no se mueve pego directamente 20 frames de estos falla algo con los frames agregados
+            salida.append(copia)
+            # i += (movimiento-freno)
+    output_video = f"videos_dados/salida_tirada_{tirada}.mp4"
+    frame_height, frame_width, _ = salida[0].shape
+    # Creo el objeto VideoWriter
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_video, fourcc, 30.0, (frame_width, frame_height))
+    # Agregar cada frame al video
+    for frame in salida:
+        out.write(cv2.cvtColor(frame,cv2.COLOR_RGB2BGR))
+    # Libero el objeto VideoWriter
+    out.release()
